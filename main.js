13ef3c97bd3013aa2e0163740974023144f9a89a
Lidime <lidimestudio@gmail.com>
2023-12-03 20:57:59 +0100

    Apañar pagina grupos

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Obtén el botón de scroll top
     var scrollTopButton = document.getElementById("scroll-top");
 
-    // Agrega un evento de clic al botón
     scrollTopButton.addEventListener("click", function() {
-        // Hace scroll hasta arriba de la página
         window.scrollTo({
             top: 0,
-            behavior: "smooth" // Desplazamiento suave
+            behavior: "smooth"
         });
     });
 });
